2076bfcd6f27cceb4aa66f0ff7e3736f46daf612
srika915404-del <srika915404@gmail.com>
2026-04-16 14:02:09 -0700

    Performed Nestedifelse conditions

---
+++ NestedifElse.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int a=11;
+int b=20;
+if(a<b){
+
+    cout<<"A is Smaller"<<endl;
+
+    if(a%2==0){
+        
+
+        cout<<"A is Even"<<endl;
+
+    }else
+    {
+        cout<<"A is Odd"<<endl;
+
+    }
+}
+    return 0;
+}
