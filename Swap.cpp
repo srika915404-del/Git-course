6cd41219fbad77eb387db6569c5056901da0f49f
srika915404-del <srika915404@gmail.com>
2026-04-16 00:31:43 -0700

    swapping of Numbers Using Cpp

---
+++ Swap.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+int a = 25;
+int b = 45;
+int temp;
+
+temp = a;
+a = b;
+ b = temp;
+
+ cout<<"a is "<<a<<" b is "<<b;
+
+
+    return 0;
+}
