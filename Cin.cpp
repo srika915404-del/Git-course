51d529ee347204646b8f320afec8d10940440a5e
srika915404-del <srika915404@gmail.com>
2026-04-15 23:55:18 -0700

    Cin input from the user

---
+++ Cin.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+using namespace std;
+
+int main ()
+
+{
+
+    /*int a;
+    cout<<"Enter a number?";
+    cin>>a;
+    cout <<"The Number You have Entered is " <<a;*/
+
+    string g;
+    cout<<"what is your Name?";
+    getline(cin,g);
+    cout<<"Hi,"<<g;
+    
+//int a = 10;
+//float b = 3.14;
+//char c = 's';
+//string d = "Srikanth Gurram";
+
+//cout<<a<<"\n"<<b<<"\n"<<c<<endl<<d;
+
+
+    return 0;
+}
