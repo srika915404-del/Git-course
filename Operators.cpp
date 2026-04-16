bbe48148a8dcfe7e00c1b5fd2b55fd26eecc5f18
srika915404-del <srika915404@gmail.com>
2026-04-16 00:29:01 -0700

    Operators Usage in Cpp

---
+++ Operators.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+
+{
+ int a = 30;
+ int b = 25;
+ int c;
+  c = a+b;
+  cout<<c<<endl;
+
+  c = a-b;
+  cout<<c<<endl;
+
+  c = a*b;
+  cout<<c<<endl;
+
+  c = a/b;
+  cout<<c<<endl;
+
+  c = a%b;
+  cout<<c<<endl;
+
+  //unary operator
+  c = -a;
+  cout<<c<<endl;
+
+  //assignment operator
+
+  c = a;
+  cout<<c<<endl;
+
+  c = 0;
+  c += 1;
+ cout<<c<<endl;
+
+    return 0;
+}
